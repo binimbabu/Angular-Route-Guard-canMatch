7223932ad103428ca4d570e0b2eaee3669ea35b3
Bini Babu <bini.babu@dbizsolution.com>
2025-07-05 19:41:17 +0530

    canMatch route guard

--- src/app/rolemanager.guard.ts
+++ src/app/rolemanager.guard.ts
@@ -2,7 +2,6 @@ import { CanMatchFn } from '@angular/router';
 
 export const rolemanagerGuard: CanMatchFn = (route, segments) => {
   var role = route.data?.['role'];
-  console.log('bini', role, route);
   if (role === 'ADMIN') {
     return true;
   }
